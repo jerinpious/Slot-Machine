4b21d5eeae30e1567be64a410e086692f2614b44
jerinpious <jpious4925@conestogac.on.ca>
2023-09-20 16:22:15 -0400

    slot machine

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
+import random
+
 MAX_LINES=3
 MAX_BET=100
 MIN_BET=1
+
+ROWS = 3
+COLS = 3
+
+symbol_count= {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine_pick(rows,cols,symbols):
+    all_symbols=[]
+    for symbols,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbols)
+
+    columns=[]
+    for col in range(cols):
+        column=[]
+        current_symbols=all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i,column in enumerate(columns):
+            if i!= len(columns)-1:
+                print(column[row], end=" | ")
+        else:
+            print(column[row],end="\n")
 
 def deposit():
     while True:
@@ -60,6 +99,7 @@ def main():
             break
             
     print(f"You have betted an amount of {bet} on {lines} lines. Your total bet is {total_bet}")
-    
+    slots=get_slot_machine_pick(ROWS,COLS,symbol_count)
+    print_slot_machine(slots)
 
 main()
